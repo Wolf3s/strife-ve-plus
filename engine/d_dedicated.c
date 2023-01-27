cc0d40144578b1f655bdc9734ff103ff57c69dba
Wolf3s <75867486+wolfysdl@users.noreply.github.com>
2023-01-27 15:50:14 -0300

    Compiling on msvc2022 but not working

---
+++ engine/d_dedicated.c
@@ -0,0 +1,29 @@
+//
+// Copyright(C) 2005-2014 Simon Howard
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// Code specific to the standalone dedicated server.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+#include "config.h"
+
+#include "m_argv.h"
+#include "net_defs.h"
+
+#include "net_dedicated.h"
+#include "net_server.h"
+#include "z_zone.h"
+
